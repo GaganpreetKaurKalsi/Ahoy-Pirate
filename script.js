7c2cf54ff8481d853338214b2b4fffe101d3ac8c
Gaganpreet Kaur Kalsi <54144759+GaganpreetKaurKalsi@users.noreply.github.com>
2020-12-10 01:40:39 +0530

    completed

---
+++ script.js
@@ -0,0 +1,21 @@
+var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var actualUrl = "https://api.funtranslations.com/translate/pirate.json"
+
+var inputText = document.querySelector(".input");
+var translateBtn = document.querySelector(".btn-primary");
+var outputDiv = document.querySelector(".output");
+
+function errorHandler(error){
+    console.log("Error occured : " + error);
+}
+
+function getURL(text){
+    return actualUrl + "?text=" + text;
+}
+
+function clickHandle(){
+    var text = inputText.value;
+    fetch(getURL(text)).then(response => response.json()).then(json => outputDiv.innerHTML = json.contents.translated).catch(errorHandler);
+}
+
+translateBtn.addEventListener("click", clickHandle);
